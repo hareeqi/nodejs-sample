3ac690d441e475e3289cea4430b6c290f7b0fe44
hareeqi <hareeqi@users.noreply.github.com>
2019-04-10 22:04:45 +0300

    Update and rename server.js to app.js

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ function handle(request, response) {
   response.end("Hello Node.js Server!");
 }
 
-http.createServer(handle).listen(process.env.PORT || 3000);
+http
+  .createServer(handle)
+  .listen(process.env.PORT || 3000, () => console.log("running"));
+
