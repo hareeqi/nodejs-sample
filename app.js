a516e7d6f79ed24426ba46b7302e22049e90632a
hareeqi <hareeqi@users.noreply.github.com>
2019-04-10 22:58:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ function handle(request, response) {
   response.end("Hello Node.js Server!");
 }
 
-http.createServer(handle).listen(process.env.PORT || 8081);
+http.createServer(handle).listen(process.env.PORT || 3000);
 
+console.log('server is working')
